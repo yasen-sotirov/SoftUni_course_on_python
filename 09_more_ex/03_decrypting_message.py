d7ebedf2db0be33d873954f0739586b33b4a8377
Yasen Sotirov <108167481+ysotirov@users.noreply.github.com>
2022-09-30 14:44:40 +0300

    add new tasks

--- 09_more_ex/03_decrypting_message.py
+++ 09_more_ex/03_decrypting_message.py
@@ -1,7 +1,7 @@
 decrypting_key = int(input())
 number_of_lines = int(input())
 
-for _ in range(number_of_lines):
+for input_lines in range(number_of_lines):
     current_letter = input()
     letter_code = ord(current_letter)
     decrypted_letter = letter_code + decrypting_key
