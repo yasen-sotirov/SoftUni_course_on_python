dcd342f02fa8b3d38506a413ce5d4fe3940ca377
ysotirov <y.sotirov@abv.bg>
2022-11-14 07:47:35 +0200

    new

--- 02_fundamentals/25_exercise/12_.py
+++ 02_fundamentals/25_exercise/12_.py
@@ -1,3 +1,35 @@
+"""" You will be receiving lines in the following format:
+"{username}-{language}-{points}" until you receive "exam finished".
+You should store each username and their submissions and points.
+If a student has two or more submissions for the same language,
+save only his maximum points.
+You can receive a command to ban a user for cheating in the following format:
+"{username}-banned". In that case, you should remove the user from the contest
+but preserve his submissions in the total count of submissions for each language.
+After receiving "exam finished", print each of the participants in the following format:
+"Results:
+{username1} | {points}
+{username2} | {points}
+…
+{usernameN} | {points}"
+After that, print each language used in the exam in the following format:
+"Submissions:
+{language1} - {submissions_count}
+{language2} - {submissions_count}
+…
+{language3} - {submissions_count}"
+Input / Constraints
+Until you receive "exam finished" you will be receiving participant submissions
+in the following format: "{username}-{language}-{points}"
+You can receive a ban command -> "{username}-banned"
+The points of the participant will always be a valid integer in the range [0-100];
+Output
+•	Print the exam results for each participant
+•	After that, print each language in the format shown above
+•	Allowed working time / memory: 100ms / 16MB
+"""
+
+
 student_dictionary = {}
 banned_students = []
 language_dict = {}
--- Tutorials/00_links.py
+++ Tutorials/00_links.py
@@ -1,2 +1,5 @@
 # https://github.com/chebishev/softUni/tree/main/Programming%20fundamentals%20Python%20September%2022
-" на Чебишев (Spoon-a) всичките задачи"
+# на Чебишев (Spoon-a) всичките задачи
+
+# https://judge.softuni.org/Contests#!/List/ByCategory/153/Fundamentals-Exams
+# упражнения
