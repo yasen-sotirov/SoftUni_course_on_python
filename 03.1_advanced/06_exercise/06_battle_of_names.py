46e449dda9f50e6d59e25a68f0f67d2da2b4f866
Yasen Sotirov <108167481+ysotirov@users.noreply.github.com>
2023-01-19 17:45:24 +0200

    add new tasks

--- 03.1_advanced/06_exercise/06_battle_of_names.py
+++ 03.1_advanced/06_exercise/06_battle_of_names.py
@@ -2,16 +2,23 @@ number_n = int(input())
 
 odd_set = set()
 even_set = set()
-sum_odd = 0
-sum_even = 0
+odd_sum = 0
+even_sum = 0
 
 for row in range(1, number_n + 1):
     name_sum = sum([ord(el) for el in input()]) // row
     if name_sum % 2 == 0:
         even_set.add(name_sum)
-        sum_even += name_sum
+        even_sum += name_sum
     else:
         odd_set.add(name_sum)
-        sum_odd += name_sum
+        odd_sum += name_sum
 
+if even_sum == odd_sum:
+    result = odd_set.union(even_set)
+elif even_sum > odd_sum:
+    result = odd_set.symmetric_difference(even_set)
+else:
+    result = odd_set.difference(even_set)
 
+print(*result, sep=', ')
