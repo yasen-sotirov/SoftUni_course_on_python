0ab2071c2250258485fbf2b42b8b958290f5a36f
Yasen Sotirov <108167481+ysotirov@users.noreply.github.com>
2023-01-24 17:46:46 +0200

    add new tasks

---
+++ TOOLS/11_FUNCTIONS.py
@@ -0,0 +1,11 @@
+"ФУНКЦИИ"  # advanced
+
+
+
+"**KVARGS"
+# def get_info(**kwargs):                     # ОПАКОВА В РЕЧНИК
+#     return f"This is {kwargs.get('name')} from {kwargs.get('town')} " \
+#            f"and he is {kwargs.get('age')} years old"
+#
+# print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))  # РАЗОПАКОВА РЕЧНИК
+# print(get_info(name="George", town="Sofia", age=20))
