76474fc2854e3452596d0ad04b2535b0b7fdc2a2
Yasen Sotirov <108167481+ysotirov@users.noreply.github.com>
2023-06-30 07:55:10 +0300

    add new tasks

--- 03.1 ADVANCED/08_multidinebsional_lists/01_sum_matrix_element.py
+++ 03.1 ADVANCED/08_multidinebsional_lists/01_sum_matrix_element.py
@@ -3,8 +3,8 @@ Write a program that reads a matrix from the console and prints:
 •	The sum of all numbers in the matrix
 •	The matrix itself
 On the first line, you will receive the matrix sizes in the format
-"{rows}, {columns}". On the next rows, you will get elements for each column separated
-by a comma and a space ", ".
+"{rows}, {columns}". On the next rows, you will get elements for each
+column separated by a comma and a space ", ".
 
 Examples
     Input
